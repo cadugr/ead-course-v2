9e9ba230f8ad71997b1b1c98a21092293d433a20
cadugr <cadu.guerra@gmail.com>
2022-06-06 08:24:43 -0300

    Mapear State Transfer Pattern com Replicação Parcial de Dados

--- src/main/java/com/ead/course/models/CourseModel.java
+++ src/main/java/com/ead/course/models/CourseModel.java
@@ -13,6 +13,9 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -65,6 +68,13 @@ public class CourseModel implements Serializable {
 	//a jpa utiliza o FetchMode JOIN como default se não definirmos o mesmo
 	//@OnDelete(action = OnDeleteAction.CASCADE) //desta forma o delete em cascata é delegado ao banco de dados
 	private Set<ModuleModel> modules;
+	
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "TB_COURSES_USERS",
+				joinColumns = @JoinColumn(name = "course_id"),
+				inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private Set<UserModel> users;
 	
 
 }
--- src/main/java/com/ead/course/models/UserModel.java
+++ src/main/java/com/ead/course/models/UserModel.java
@@ -1,13 +1,18 @@
 package com.ead.course.models;
 
 import java.io.Serializable;
+import java.util.Set;
 import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,6 +30,25 @@ public class UserModel implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	//Não precisamos gerar o id, pois ele será passado pelo microservice de authuser por meio de state transfer
 	private UUID id;
+	@Column(nullable = false, unique = true, length = 50)
+	private String email;
+	@Column(nullable = false, length = 150)
+	private String fullName;
+	@Column(nullable = false)
+	//Estamos utilizando como String ao invés de enum para não manter um vínculo forte com authuser, pois se mudar o enum lá, tem que mudar aqui
+	//e também pois não precisamos seguir fielmente a tabela de usuários de authuser
+	private String userStatus;
+	@Column(nullable = false)
+	private String userType;
+	@Column(length = 20)
+	private String cpf;
+	@Column
+	private String imageUrl;
+	
+	@ManyToMany(mappedBy = "users", fetch = FetchType.LAZY)
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	private Set<CourseModel> courses;
 
 }
